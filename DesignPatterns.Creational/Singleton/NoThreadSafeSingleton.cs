a903417590cb9c5c6518b15473ccc6199665439b
Jhonny Horacio Crespo <jhonny.h.cr@gmail.com>
2018-04-14 16:08:35 -0400

    Add project files.

---
+++ DesignPatterns.Creational/Singleton/NoThreadSafeSingleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Singleton
+{
+    public sealed class NoThreadSafeSingleton
+    {
+        private NoThreadSafeSingleton()
+        {
+            
+        }
+
+        public static NoThreadSafeSingleton instance;
+
+        public static NoThreadSafeSingleton Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new NoThreadSafeSingleton();
+                }
+
+                return instance;
+            }
+        }
+    }
+}
---
+++ DesignPatterns.Creational/Singleton/ThreadSafeSingleton.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Singleton
+{
+    public sealed class ThreadSafeSingleton
+    {
+        private ThreadSafeSingleton()
+        {
+            
+        }
+
+        private static readonly object padlock = new object();
+        private static ThreadSafeSingleton instance;
+
+        public static ThreadSafeSingleton Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new ThreadSafeSingleton();
+                    }
+
+                    return instance;
+                }
+            }
+        }
+    }
+}
